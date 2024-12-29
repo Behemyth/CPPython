79a7ce8ea726af6745594234294d6ff5eadc5eaf
Asher Norland <asher.norland@gmail.com>
2024-12-29 17:55:21 -0500

    Update schema.py

--- cppython/test/schema.py
+++ cppython/test/schema.py
@@ -2,7 +2,7 @@
 
 from typing import Annotated
 
-from pydantic import Field
+from pydantic import DirectoryPath, Field
 
 from cppython.core.schema import CPPythonModel
 
@@ -11,6 +11,10 @@ class Variant[T: CPPythonModel](CPPythonModel):
     """A configuration variant for a schema type"""
 
     configuration: Annotated[T, Field(description='The configuration data')]
+    directory: Annotated[
+        DirectoryPath | None,
+        Field(description='The directory to mount alongside the configuration. `tests/build/<directory>`'),
+    ] = None
 
 
 class Variants[T: CPPythonModel](CPPythonModel):
